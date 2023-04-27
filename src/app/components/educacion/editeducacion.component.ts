83f222e26e3cc57f1730391bc6e1f4807301867f
gaston <gastonj941@gmail.com>
2023-04-27 17:27:28 -0300

    corregido edit educacion

--- src/app/components/educacion/editeducacion.component.ts
+++ src/app/components/educacion/editeducacion.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Educacion } from 'src/app/model/educacion';
+import { EducacionService } from 'src/app/service/educacion.service';
 
 @Component({
   selector: 'app-editeducacion',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./editeducacion.component.css']
 })
 export class EditeducacionComponent {
+  educacion: Educacion = null;
+  
+  constructor(
+    private educacionS: EducacionService,
+    private activatedRouter : ActivatedRoute,
+    private router: Router
+  ) { }
 
+  ngOnInit(): void {
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.educacionS.detail(id).subscribe({
+      next: (data) =>{
+        this.educacion = data;
+      }, error: () =>{
+         alert("Error al modificar");
+         this.router.navigate(['']);
+      }
+    }
+    )
+  }
+
+  onUpdate(): void{
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.educacionS.update(id, this.educacion).subscribe({
+      next: () => {
+        this.router.navigate(['']);
+      }, error: () => {
+        alert("Error al modificar la educacion");
+        this.router.navigate(['']);
+      }
+    }
+    )
+  }
 }
